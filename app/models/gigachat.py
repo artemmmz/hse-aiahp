7c682ea0f1d9a76285a4087fda089795434c4aa0
Artem Zlobin <arzar0046@gmail.com>
2024-10-14 17:46:06 +0300

    Add Gigachat model

---
+++ app/models/gigachat.py
@@ -0,0 +1,121 @@
+import json
+from datetime import datetime, timedelta
+from enum import Enum
+from typing import Optional
+
+import requests
+
+from app.models.base import BaseModel
+
+
+class GigachatModels(str, Enum):
+    LITE = 'GigaChat'
+    PRO = 'GigaChat-Pro'
+
+
+class GigachatScopes(str, Enum):
+    PERSON = 'GIGACHAT_API_PERS'
+    B2B = 'GIGACHAT_API_B2B'
+    CORPORATION = 'GIGACHAT_API_CORP'
+
+
+class Gigachat(BaseModel):
+    def __init__(
+            self,
+            token: str,
+            model: GigachatModels = GigachatModels.LITE,
+            scope: GigachatScopes = GigachatScopes.PERSON,
+            system_prompt: Optional[str] = None,
+            timeout: Optional[int] = 5000,
+    ):
+        """
+        Initialize the Gigachat model.
+        :param token: Gigachat API token
+        :param model: Gigachat model
+        :param scope: Gigachat scope
+        :param system_prompt: System prompt
+        :param timeout: Timeout
+        """
+        super().__init__(system_prompt)
+        self.__token = token
+        self.__model = model
+        self.__scope = scope
+        self.timeout = timeout
+
+        self.__access_token = None
+        self.token_expires = 0
+
+        self.__update_access_key()
+
+    def __update_access_key(self) -> None:
+        """
+        Update access key. USE ONLY AFTER INITIALIZATION
+        """
+        url = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth'
+        data = f'scope={self.__scope}'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+            'Authorization': 'Basic authorization_key',
+            'RqUID': self.__token,
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+
+        if response.status_code != 200:
+            return
+
+        result = response.json()
+
+        self.token_expires: int = result['expires']
+        self.__access_token: str = result['__access_token']
+
+    def __check_access_key(self) -> None:
+        """
+        Check and update access_token if needed
+        """
+        timedout_now = datetime.now() + timedelta(seconds=self.timeout)
+        if timedout_now.timestamp() >= self.token_expires - 1:
+            self.__update_access_key()
+
+    def ask(self, user_message: str, clear_history: bool = True) -> Optional[str]:
+        """
+        Send a message to the assistant and return the assistant's response.
+        :param user_message: User message
+        :param clear_history: True if history should be cleared
+        """
+        if clear_history:
+            self.messages = []
+        if len(self.messages) == 0 and self.system_prompt:
+            self.messages.append({"role": "system", "content": self.system_prompt})
+
+        self.messages.append({"role": "user", "content": user_message})
+        self.__check_access_key()
+
+        url = 'https://gigachat.devices.sberbank.ru/api/v1/chat/completions'
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {self.__access_token}',
+        }
+        json_request = {
+            'model': self.__model,
+            'messages': self.messages,
+            'stream': False,
+            'update_interval': 0
+        }
+
+        response = requests.post(
+            url=url,
+            headers=headers,
+            data=json.dumps(json_request),
+            timeout=self.timeout
+        )
+        if response.status_code != 200:
+            return None
+
+        result = response.json()
+        assistant_content = result['choices'][0]['message']['content']
+        self.messages.append({"role": "assistant", "content": assistant_content})
+
+        return assistant_content
